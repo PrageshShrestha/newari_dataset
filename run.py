dbafe00df7034c034d2674cf5742c6e6aa160107
PrageshShrestha <binormus006@gmail.com>
2026-06-03 20:25:24 +0545

    fix:editted actual output tokens

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ repo = "./tokenizer_dir"
 tok = AutoTokenizer.from_pretrained(repo, trust_remote_code=True)
 
 prompt = "नेपालको राजधानी "
-ids = tok(prompt, return_tensors="pt").input_ids.to(device)
+smthing = tok(prompt,return_tensors="pt").to(device)
+print(smthing)
+ids = smthing.input_ids
 print(prompt)
 print(ids)
